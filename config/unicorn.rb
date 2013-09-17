03fa8b5375d8151e4180ceab4f706da3448f4170
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-09-17 10:35:55 -0500

    capistrano and unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -37,8 +37,8 @@ pid "/home/red-innovadores/apps/ndi_lac/current/tmp/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/red-innovadores/apps/ndi_lac/current/shared/logs/unicorn.stderr.log"
-stdout_path "/home/red-innovadores/apps/ndi_lac/current/shared/logs/unicorn.stdout.log"
+stderr_path "/home/red-innovadores/apps/ndi_lac/current/tmp/logs/unicorn.stderr.log"
+stdout_path "/home/red-innovadores/apps/ndi_lac/current/tmp/logs/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
