85f2c8f9ee908c1fc2d193b3a423a087fe4c02e4
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-09-19 09:16:23 -0500

    changing sock name

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ working_directory "/home/red-innovadores/apps/ndi_lac/current" # available in 0.
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/.sock", :backlog => 64
+listen "/tmp/ndi_lac.sock", :backlog => 64
 listen 3500, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
