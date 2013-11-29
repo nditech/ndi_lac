598aba265cb90669cdfab97846df5f502017c0d4
Jorge Pardiñas <jparbros@gmail.com>
2013-11-29 13:37:06 -0600

    helper KaminariHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  helper KaminariHelper
+  
   before_filter do
     resource = controller_name.singularize.to_sym
     method = "#{resource}_params"
